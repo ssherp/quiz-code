398f6b18d05a806439a1a879eda07eba0ba3b274
ssherp <ssherpa89@gmail.com>
2023-07-05 15:59:58 -0700

    add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,40 @@
 //global variables
 var score=0;
-var timeLeft=500;
+var timeLeft=100;
 var questions = [
     {
-        question: "what is the capital of Urugauy?",
-        choices: ["Manangua", "Santiago", "Monte Video", "Brasilia"],
-        answer: 'Brasilia'
+        question: "How can a datatype be declared to be a constant type?",
+        choices: ["const", "var", "let", "constant"],
+        answer: 'const'
     },
     {
-        question: "1+1",
-        choices: [11, 2, 5, 4],
-        answer: 2
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choices: ["stringify","parse","convert", "element"],
+        answer: "stringify"
     },
+    {
+        question: "Which of the following is not a Javascript framework?",
+        choices:["node","vue","react","cassandra"],
+        answer:"cassandra"
+    },
+    {
+        question:"Which of the following scoping type does JavaScript use?",
+        choices:["sequential","segmental","lexical","literal"],
+        answer:"lexical"
+    }
+    {
+        question:"Which of the following tag is used to insert a line-break in HTML?",
+        choices:["br","a","pre","b"],
+        answer:"br"
+    }
 ]
 // // var questions=["what is the capital of Urugauy?","what is the capital of panama?"];
 // var answerChoices =[["Manangua", "Santiago", "Monte Video", "Brasilia"],[""]]; //made of arry of strings
 // // answerChoices[0][0]
-// var correctAnswer= []; //made of strings
-
-var startButton= document.getElementById ("start-button")
-var displayInit= document.querySelector ("#displayStart")
-var displayQuestion=document.querySelector("#display-questions")
+//all of our selectors
+var startButton= document.getElementById ("start-button");
+var displayInit= document.querySelector ("#displayStart");
+var displayQuestion=document.querySelector("#display-questions");
 var currentQuestions=0;
 
 //all of our selectors
@@ -37,24 +51,26 @@ function gameStart()  {
      quizTimer = setInterval(function() {
         timeLeft--;
         console.log(timeLeft);
-        timeEl.textContent = "count down: "+timeLeft
+        timeEl.textContent = "countdown: "+timeLeft;
         if (timeLeft <= 0){
           endGame();  
         }
-    }, 1000)
-    displayInit.style.display="none"
+    }, 1000);
+    displayInit.style.display="none";
  //set interval
  //once the timer hits 0, call endGame function
     //hide the start button
 
     //reveal the option, questions 
-    nextQuestion()
+    showQuestion();
 }
 
-function nextQuestion(){
+function showQuestion(){
     displayQuestion.textContent=questions[0].question
-    
-    
+    var buttonEl_1 = document.createElement('button');
+    buttonEl_1.textContent = questions[0].choices[0]
+    // appendChild
+    choices.append(buttonEl_1)
     
     
 }
